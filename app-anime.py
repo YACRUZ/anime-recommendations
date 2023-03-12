ef83badde2b9a4e66d088b2c16fbfad3c8c5f6a6
YACRUZ <yamatsukino@gmail.com>
2023-03-12 11:04:35 -0600

    add csv

--- app-anime.py
+++ app-anime.py
@@ -1,3 +1,16 @@
 import streamlit as st 
+import pandas as pd 
 
-st.title('Hello world')
+st.title('Anime recommendations')
+DATA_URL = ('anime.csv')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    return data   
+
+data_load_state = st.text('Loading data...')
+data = load_data(1000)
+data_load_state.text('Done ! using cache...')
+
+st.dataframe(data)
